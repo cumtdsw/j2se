e68d9e8f18a701968d098652bc441731a42a22c9
pugwoo <pugwoo@gmail.com>
2013-05-08 22:46:03 +0800

    netty

---
+++ net/netty/netty/hello/HelloWorldServer.java
@@ -0,0 +1,39 @@
+package netty.hello;
+
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
+
+import org.jboss.netty.bootstrap.ServerBootstrap;
+import org.jboss.netty.channel.ChannelPipeline;
+import org.jboss.netty.channel.ChannelPipelineFactory;
+import org.jboss.netty.channel.Channels;
+import org.jboss.netty.channel.socket.nio.NioServerSocketChannelFactory;
+import org.jboss.netty.handler.codec.string.StringDecoder;
+import org.jboss.netty.handler.codec.string.StringEncoder;
+
+/**
+ * 2012年11月10日 下午04:24:20
+ */
+public class HelloWorldServer {
+
+	public static void main(String[] args) {
+
+		ServerBootstrap bootstrap = new ServerBootstrap(
+				new NioServerSocketChannelFactory(Executors
+						.newCachedThreadPool(), Executors.newCachedThreadPool()));
+		bootstrap.setPipelineFactory(new ChannelPipelineFactory() {
+			@Override
+			public ChannelPipeline getPipeline() throws Exception {
+				ChannelPipeline pipeline = Channels.pipeline();
+				pipeline.addLast("decoder", new StringDecoder());
+				pipeline.addLast("encoder", new StringEncoder());
+				// 一般只需要在这里修改一下
+				pipeline.addLast("handler", new HelloWorldServerHandler());
+				return pipeline;
+			}
+		});
+
+		bootstrap.bind(new InetSocketAddress(8787));
+
+	}
+}
---
+++ net/netty/readme.txt
@@ -0,0 +1,7 @@
+介绍PPT:
+http://www.searchtb.com/2011/03/dive_into_netty.html
+
+
+完整的入门教材：
+http://blog.csdn.net/vba_2001/article/details/7307125
+服务端程序通常的处理过程是：解码请求数据、业务逻辑处理、编码响应。
