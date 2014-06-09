78491a60573b04c192e479d0c02e27758a8e2cf8
pugwoo <pugwoo@gmail.com>
2014-06-09 10:34:04 +0800

    commons io copy stream

---
+++ io/common.io/test/org/apache/common/io/TestCopyFile.java
@@ -0,0 +1,27 @@
+package test.org.apache.common.io;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.apache.commons.io.IOUtils;
+
+/**
+ * 4种方式复制文件：
+ * http://examples.javacodegeeks.com/core-java/io/file/4-ways-to-copy-file
+ * -in-java/
+ * 
+ * 根据这篇博客的测试，Channel复制文件是最快的。
+ * 
+ * 2014年6月9日 10:24:34
+ */
+public class TestCopyFile {
+
+	public static void main(String[] args) throws IOException {
+		FileInputStream inputStream = new FileInputStream("fromFile.txt");
+		FileOutputStream outputStream = new FileOutputStream("toFile.txt");
+		int bytes = IOUtils.copy(inputStream, outputStream);
+		System.out.println("copy bytes:" + bytes);
+	}
+
+}
