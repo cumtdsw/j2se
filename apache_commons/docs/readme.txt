43a0d35e949eb22a70dc08d943dc42294961ecaa
pugwoo <pugwoo@gmail.com>
2014-10-10 19:11:06 +0800

    apache commons pratice init

---
+++ apache_commons/docs/readme.txt
@@ -0,0 +1,4 @@
+apache commons常用：
+
+commons lang
+// 目前已经是版本3+，但是老项目多是版本2
---
+++ apache_commons/src/main/java/com/pugwoo/j2se/apache_commons/lang/ExampleBean.java
@@ -0,0 +1,57 @@
+package com.pugwoo.j2se.apache_commons.lang;
+
+import java.util.Date;
+
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+/**
+ * 演示通用的通过反射来输出java bean的所有属性 2014年10月10日 19:06:41
+ */
+public class ExampleBean {
+
+	private String name;
+	private int age;
+	private Date registerTime;
+
+	@Override
+	public String toString() {
+		// 使用apache commons lang，可以设置不同的输出格式
+		return ReflectionToStringBuilder.toString(this,
+				ToStringStyle.SHORT_PREFIX_STYLE);
+	}
+
+	public static void main(String[] args) {
+		ExampleBean bean = new ExampleBean();
+		bean.setName("nick");
+		bean.setAge(27);
+		bean.setRegisterTime(new Date());
+
+		System.out.println(bean);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public Date getRegisterTime() {
+		return registerTime;
+	}
+
+	public void setRegisterTime(Date registerTime) {
+		this.registerTime = registerTime;
+	}
+
+}
