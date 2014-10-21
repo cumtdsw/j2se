86c3e2ab52f30ef95ff07767f847025cc95b7ab3
pugwoo <pugwoo@gmail.com>
2014-10-21 15:44:51 +0800

    net使用新版，同时整理出Test程序

---
+++ net/httpclient/com/pugwoo/test/TestNet.java
@@ -0,0 +1,73 @@
+package com.pugwoo.test;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.http.HttpHost;
+import org.apache.http.client.ClientProtocolException;
+import org.junit.Test;
+
+import com.pugwoo.utils.Net;
+import com.pugwoo.utils.Net.HttpRespContent;
+
+/**
+ * 2014年10月21日 13:47:59 测试我的utils.Net，迁移自Get.java
+ */
+public class TestNet {
+
+	/**
+	 * 演示获取普通的网页
+	 * 
+	 * @throws ClientProtocolException
+	 * @throws IOException
+	 */
+	@Test
+	public void testGet() throws ClientProtocolException, IOException {
+		HttpHost proxy = null;
+		// proxy = new HttpHost("127.0.0.1", 8888, "http");
+
+		String url = "http://www.baidu.com/";
+		HttpRespContent content = Net.get(url, null, proxy, null);
+
+		System.out.println(content.content);
+		System.out.println(content.content.getBytes().length + "字节");
+	}
+
+	/**
+	 * 测试下载文件,二进制
+	 * 
+	 * @throws IOException
+	 * @throws ClientProtocolException
+	 */
+	@Test
+	public void testDownload() throws ClientProtocolException, IOException {
+		HttpHost proxy = null;
+		// proxy = new HttpHost("127.0.0.1", 8888, "http");
+
+		String img_url = "http://www.baidu.com/img/bdlogo.png";
+		FileOutputStream out = new FileOutputStream("C:/baidu_logo.png");
+		Net.get(img_url, null, proxy, out);
+	}
+
+	/**
+	 * 演示post登录，需要本地开一个登录应用例子
+	 * 
+	 * @throws IOException
+	 * @throws ClientProtocolException
+	 */
+	@Test
+	public void testPostLogin() throws ClientProtocolException, IOException {
+		String post_url = "http://127.0.0.1:8080/userRegister/login";
+
+		Map<String, String> params = new HashMap<String, String>();
+		params.put("username", "nick");
+		params.put("password", "123456");
+
+		HttpRespContent post_content = Net.post(post_url, params, null, null,
+				null);
+		System.out.println(post_content.content);
+	}
+
+}
--- net/httpclient/com/pugwoo/utils/Net.java
+++ net/httpclient/com/pugwoo/utils/Net.java
@@ -3,152 +3,172 @@ package com.pugwoo.utils;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.http.Header;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpHost;
-import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
+import org.apache.http.StatusLine;
 import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.conn.params.ConnRoutePNames;
-import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 
 /**
- * 2012-12-3 下午03:23:35
- * http get數據
+ * 2012-12-3 下午03:23:35 http get數據
  * 
- * 2013年11月30日 22:41:21
- * 由于网络问题，增加重试机制，增加成功率
+ * 2013年11月30日 22:41:21 由于网络问题，增加重试机制，增加成功率
+ * 
+ * 2014年10月21日 13:48:21 去掉重试机制，交给上一层去完成
+ * 
+ * 2014年10月21日 14:52:51 使用新版4.3.5的httpclient，并修改GET和POST方法
  */
 public class Net {
-	
-	public static int retryTimes = 100;
-	
-	public static String get(String url, String encode, HttpHost proxy,
-			FileOutputStream out) throws Exception {
-		Exception ex = null;
-		for(int tries = 0; tries < retryTimes; tries++) {
-			try {
-				String content = __get(url, encode, proxy, out);
-				return content;
-			} catch (Exception e) {
-				Thread.sleep(3000 * tries);
-				ex = e;
-			}
-		}
-		if(ex != null) {
-			throw ex;
-		}
-		return null;
+
+	// 2014年10月21日 Chrome抓取
+	private static String USER_AGENT_CHROME = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.110 Safari/537.36";
+
+	public static class HttpRespContent {
+		public StatusLine statusLine; // html状态
+		public Header[] headers; // html头部
+		public String content; // html正文
 	}
 
 	/**
-	 * 從url獲取數據
-	 * 1.如果encode不為null，則使用編碼，否則不使用編碼
-	 * 2.如果代理proxy不為null，則使用proxy
-	 * 3.如果out不為null，則寫入到文件中，此時返回null；如果out為null，則返回string
+	 * 從url獲取數據<br>
+	 * 1.如果encode不為null，則使用編碼，否則不使用編碼<br>
+	 * 2.如果代理proxy不為null，則使用proxy<br>
+	 * 3.如果out不為null，則http content寫入到文件中，仅返回头数据；<br>
+	 * 如果out為null，則返回完整的HttpRespContent
 	 */
-	private static String __get(String url, String encode, HttpHost proxy,
-			FileOutputStream out) throws ClientProtocolException, IOException {
+	public static HttpRespContent get(String url, String encode,
+			HttpHost proxy, FileOutputStream out)
+			throws ClientProtocolException, IOException {
 
-		DefaultHttpClient httpclient = new DefaultHttpClient();
-		httpclient.getParams().setIntParameter("http.socket.timeout",10000); //毫秒
-		String content = null;
+		CloseableHttpClient httpclient = HttpClients.createDefault();
+		// 配置超时时间和代理
+		RequestConfig requestConfig = RequestConfig.custom()
+				.setSocketTimeout(10000).setConnectTimeout(10000)
+				.setConnectionRequestTimeout(10000).setProxy(proxy).build();
+
+		CloseableHttpResponse response = null;
+		HttpRespContent httpRespContent = new HttpRespContent();
 
 		try {
-			if (proxy != null) {
-				httpclient.getParams().setParameter(
-						ConnRoutePNames.DEFAULT_PROXY, proxy);
-			}
+			HttpGet httpGet = new HttpGet(url);
+			httpGet.setConfig(requestConfig);
+			httpGet.setHeader("User-Agent", USER_AGENT_CHROME);
 
-			HttpGet req = new HttpGet(url);
-			
-			req.setHeader("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_4) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.46 Safari/536.5");
-			
-			HttpResponse rsp = httpclient.execute(req);
+			response = httpclient.execute(httpGet);
 
-			// 獲得html頁面源碼
-			HttpEntity entity = rsp.getEntity();
+			httpRespContent.statusLine = response.getStatusLine();
+			httpRespContent.headers = response.getAllHeaders();
+
+			HttpEntity entity = response.getEntity();
 			if (entity != null) {
 				if (out != null) {
 					out.write(EntityUtils.toByteArray(entity));
 				} else {
-					content = EntityUtils.toString(entity);
+					if (encode == null) {
+						httpRespContent.content = EntityUtils.toString(entity);
+					} else {
+						httpRespContent.content = EntityUtils.toString(entity,
+								encode);
+					}
 				}
 			}
 
-			// 獲得請求返回狀態
-			// rsp.getStatusLine()
-			// 獲得http頭部
-			// rsp.getAllHeaders()
-
+			// 当entity已经处理完了，关闭掉
+			EntityUtils.consume(entity);
 		} finally {
-			httpclient.getConnectionManager().shutdown();
+			// 建立的http连接，仍旧被response保持着，允许我们从网络socket中获取返回的数据
+			// 为了释放资源，我们必须手动消耗掉response或者取消连接（使用CloseableHttpResponse类的close方法）
+			if (response != null) {
+				response.close();
+			}
 		}
 
-		return content;
+		return httpRespContent;
 	}
 
-	@SuppressWarnings("unchecked")
-	public static String post(String url, Map<String, String> params,
+	/**
+	 * 支持post数据，其它说明同GET
+	 * 
+	 * @param url
+	 * @param params
+	 * @param encode
+	 * @param proxy
+	 * @param out
+	 * @return
+	 * @throws ClientProtocolException
+	 * @throws IOException
+	 */
+	public static HttpRespContent post(String url, Map<String, String> params,
 			String encode, HttpHost proxy, FileOutputStream out)
 			throws ClientProtocolException, IOException {
 
-		DefaultHttpClient httpclient = new DefaultHttpClient();
-		
-		String content = null;
+		CloseableHttpClient httpclient = HttpClients.createDefault();
+		// 配置超时时间和代理
+		RequestConfig requestConfig = RequestConfig.custom()
+				.setSocketTimeout(10000).setConnectTimeout(10000)
+				.setConnectionRequestTimeout(10000).setProxy(proxy).build();
+
+		// 拼接参数
+		List<NameValuePair> nvps = new ArrayList<NameValuePair>();
+		if (params != null) {
+			for (String key : params.keySet()) {
+				nvps.add(new BasicNameValuePair(key, params.get(key)));
+			}
+		}
+
+		CloseableHttpResponse response = null;
+		HttpRespContent httpRespContent = new HttpRespContent();
 
 		try {
-			if (proxy != null) {
-				httpclient.getParams().setParameter(
-						ConnRoutePNames.DEFAULT_PROXY, proxy);
+			HttpPost httpPost = new HttpPost(url);
+			httpPost.setConfig(requestConfig);
+			httpPost.setHeader("User-Agent", USER_AGENT_CHROME);
+			if (!nvps.isEmpty()) {
+				httpPost.setEntity(new UrlEncodedFormEntity(nvps));
 			}
 
-			HttpPost req = new HttpPost(url);
-			req.setHeader("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_4) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.46 Safari/536.5");
+			response = httpclient.execute(httpPost);
 
-			List<NameValuePair> nvps = new ArrayList<NameValuePair>();
+			httpRespContent.statusLine = response.getStatusLine();
+			httpRespContent.headers = response.getAllHeaders();
 
-			if (params != null) {
-				Iterator iter = params.entrySet().iterator();
-				while (iter.hasNext()) {
-					Map.Entry<String, String> entry = (Map.Entry<String, String>) iter
-							.next();
-					nvps.add(new BasicNameValuePair(entry.getKey(), entry
-							.getValue()));
-				}
-			}
-			
-			req.setEntity(new UrlEncodedFormEntity(nvps));
-
-			HttpResponse rsp = httpclient.execute(req);
-
-			// 獲得html頁面源碼
-			HttpEntity entity = rsp.getEntity();
+			HttpEntity entity = httpPost.getEntity();
 			if (entity != null) {
 				if (out != null) {
 					out.write(EntityUtils.toByteArray(entity));
 				} else {
-					content = EntityUtils.toString(entity);
+					if (encode == null) {
+						httpRespContent.content = EntityUtils.toString(entity);
+					} else {
+						httpRespContent.content = EntityUtils.toString(entity,
+								encode);
+					}
 				}
 			}
 
-			// 獲得請求返回狀態
-			// rsp.getStatusLine()
-			// 獲得http頭部
-			// rsp.getAllHeaders()
-
+			// 当entity已经处理完了，关闭掉
+			EntityUtils.consume(entity);
 		} finally {
-			httpclient.getConnectionManager().shutdown();
+			// 建立的http连接，仍旧被response保持着，允许我们从网络socket中获取返回的数据
+			// 为了释放资源，我们必须手动消耗掉response或者取消连接（使用CloseableHttpResponse类的close方法）
+			if (response != null) {
+				response.close();
+			}
 		}
 
-		return content;
+		return httpRespContent;
 	}
 }
