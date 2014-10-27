8acd9d0f9d576918990a935a8f223b5a997ccd2c
pugwoo <pugwoo@gmail.com>
2014-10-27 16:22:41 +0800

    rename

--- apache_commons/src/main/java/com/pugwoo/j2se/apache_commons/lang/BeanReflectToString.java
+++ apache_commons/src/main/java/com/pugwoo/j2se/apache_commons/lang/BeanReflectToString.java
@@ -8,7 +8,7 @@ import org.apache.commons.lang3.builder.ToStringStyle;
 /**
  * 演示通用的通过反射来输出java bean的所有属性 2014年10月10日 19:06:41
  */
-public class ExampleBean {
+public class BeanReflectToString {
 
 	private String name;
 	private int age;
@@ -22,7 +22,7 @@ public class ExampleBean {
 	}
 
 	public static void main(String[] args) {
-		ExampleBean bean = new ExampleBean();
+		BeanReflectToString bean = new BeanReflectToString();
 		bean.setName("nick");
 		bean.setAge(27);
 		bean.setRegisterTime(new Date());
