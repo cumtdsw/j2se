d1bcbb699bed3ac21e545013ff80c363d9c47f61
pugwoo <pugwoo@gmail.com>
2014-10-28 14:11:27 +0800

    container--

---
+++ container/google-guava/com/pugwoo/CreateCollectionUtils.java
@@ -0,0 +1,26 @@
+package com.pugwoo;
+
+import java.util.List;
+import java.util.Map;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+
+/**
+ * 一些简单的创建List Map的例子
+ */
+public class CreateCollectionUtils {
+
+	public static void main(String[] args) {
+
+		List<Long> list = Lists.newArrayList(1L, 2L, 3L);
+		System.out.println(list);
+		
+		Map<String, Long> map = Maps.newHashMap();
+		map.put("one", 1L);
+		map.put("two", 2L);
+		
+		System.out.println(map);
+	}
+
+}
--- container/map/com/pugwoo/test/TestConcurrentHashMap.java
+++ container/map/com/pugwoo/test/TestConcurrentHashMap.java
@@ -4,14 +4,19 @@ import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * 2012年2月21日
+ * 
+ * ConcurrentHashMap是线程安全的
  */
 public class TestConcurrentHashMap {
 
 	public static void main(String[] args) {
-		ConcurrentHashMap<Integer, Integer> map = new ConcurrentHashMap<Integer, Integer>();
+		ConcurrentHashMap<Integer, Integer> map = 
+				new ConcurrentHashMap<Integer, Integer>();
+		
+		// TODO 这里并没有用多线程来演示
+		
 		map.put(1, 1);
 		System.out.println(map.get(1));
-		
-		
+
 	}
 }
