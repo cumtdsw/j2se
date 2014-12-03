0336adbfffbd12293be445ece46ae124f1c74904
pugwoo <pugwoo@gmail.com>
2014-12-03 14:50:23 +0800

    zh -> maven

--- zh/src/com/pugwoo/pinyin/GetStarted.java
+++ zh/src/com/pugwoo/pinyin/GetStarted.java
@@ -5,13 +5,14 @@ import net.sourceforge.pinyin4j.PinyinHelper;
 public class GetStarted {
 
 	public static void main(String[] args) {
-		{
-			String[] pinyinArray = PinyinHelper
-					.toTongyongPinyinStringArray('李');
-			// String[] pinyinArray = PinyinHelper.toYalePinyinStringArray('李');
-			for (int i = 0; i < pinyinArray.length; i++) {
-				System.out.println(pinyinArray[i]);
-			}
+		/**
+		 * 之所以返回数组是可能为多音字
+		 */
+		String[] pinyinArray = PinyinHelper.toTongyongPinyinStringArray('李');
+		// String[] pinyinArray = PinyinHelper.toYalePinyinStringArray('李');
+		
+		for (int i = 0; i < pinyinArray.length; i++) {
+			System.out.println(pinyinArray[i]);
 		}
 	}
 
