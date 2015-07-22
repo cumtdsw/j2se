86f8e6741b69c210d6887c8fb32f2f449ed187f2
pugwoo <pugwoo@gmail.com>
2015-07-22 17:37:58 +0800

    调整目录结构

--- thread/src/com/pugwoo/benchmark/BenchNewFixedThreadPool.java
+++ thread/src/com/pugwoo/benchmark/BenchNewFixedThreadPool.java
@@ -1,10 +1,19 @@
-package com.pugwoo;
+package com.pugwoo.benchmark;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import com.pugwoo.CountTime;
+
 /**
  * 用于测试固定线程池的性能
+ * 
+ * 在主流笔记本（2015年）上测试，这个空跑的任务完全用于测试线程池调度的性能
+ * poolSize   线程/秒
+ * 1000       196万
+ * 100        167万
+ * 
+ * 所以，对于普通的应用，百万级/s的速度没有问题。
  */
 public class BenchNewFixedThreadPool {
 
@@ -17,7 +26,7 @@ public class BenchNewFixedThreadPool {
 		// 会出现各种错误，包括java.lang.OutOfMemoryError
 		// 所以要控制加入的速度
 		// 参考【BenchThreadPool】
-		int fixedThreadPoolSize = 1000;
+		int fixedThreadPoolSize = 100;
 		int threadNum = 10000000;
 		
 		/**
@@ -33,7 +42,7 @@ public static ExecutorService newFixedThreadPool(int nThreads) {
 				.newFixedThreadPool(fixedThreadPoolSize);
 
 		for (int i = 0; i < threadNum; i++) {
-			final int fi = i;
+//			final int fi = i;
 			service.execute(new Runnable() {
 				@Override
 				public void run() {
@@ -43,7 +52,7 @@ public static ExecutorService newFixedThreadPool(int nThreads) {
 		}
 		
 		service.shutdown();
-		countTime.printInMs();
+		countTime.printInMs(); // 一般是5s这样
 	}
 
 }
