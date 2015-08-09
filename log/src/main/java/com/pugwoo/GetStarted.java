25ebd63441d1ed73654a1177c5cdcc1ea31fc072
pugwoo <pugwoo@gmail.com>
2015-08-09 15:47:45 +0800

    config

--- log/src/main/java/com/pugwoo/GetStarted.java
+++ log/src/main/java/com/pugwoo/GetStarted.java
@@ -11,11 +11,10 @@ public class GetStarted {
 	// 比较常用的用法，根据类名实例化一个静态的全局日志记录器
 	private static final Logger logger = LogManager.getLogger(GetStarted.class);
 
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) {
 		logger.trace("Trace Message!");
 		logger.debug("Debug Message!");
 		logger.info("Info Message!");
-		Thread.sleep(1);
 		logger.warn("Warn Message!");
 		logger.error("Error Message!");
 		logger.fatal("Fatal Message!");
