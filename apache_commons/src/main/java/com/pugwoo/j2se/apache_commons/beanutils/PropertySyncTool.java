d1513f88bfe6ccdf08302240a49a252d22239e9a
pugwoo <pugwoo@gmail.com>
2016-03-31 09:42:28 +0800

    property utils learn more

--- apache_commons/src/main/java/com/pugwoo/j2se/apache_commons/beanutils/PropertySyncTool.java
+++ apache_commons/src/main/java/com/pugwoo/j2se/apache_commons/beanutils/PropertySyncTool.java
@@ -1,5 +1,10 @@
 package com.pugwoo.j2se.apache_commons.beanutils;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.apache.commons.beanutils.PropertyUtils;
 
 /**
@@ -9,7 +14,42 @@ import org.apache.commons.beanutils.PropertyUtils;
  * 示例详见test代码
  */
 public class PropertySyncTool {
+	
+	public static class InnerClass {
+		private InnerInnerClass innerInnerClass = new InnerInnerClass();
+		public InnerInnerClass getInnerInnerClass() {
+			return innerInnerClass;
+		}
+		public void setInnerInnerClass(InnerInnerClass innerInnerClass) {
+			this.innerInnerClass = innerInnerClass;
+		}
+	}
+	
+	public static class InnerInnerClass {
+		private String name;
+		private List<Integer> scores = new ArrayList<Integer>();
+		private Map<String, Object> map = new HashMap<String, Object>();
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public List<Integer> getScores() {
+			return scores;
+		}
+		public void setScores(List<Integer> scores) {
+			this.scores = scores;
+		}
+		public Map<String, Object> getMap() {
+			return map;
+		}
+		public void setMap(Map<String, Object> map) {
+			this.map = map;
+		}
+	}
 
+	// 如果目标obj没值则同步，有值则不变的方法
 	public static void setPropertyIfTargetNull(Object orgin, Object target,
 			String propertyName) throws Exception {
 		Object value = PropertyUtils.getProperty(target, propertyName);
@@ -19,6 +59,22 @@ public class PropertySyncTool {
 				PropertyUtils.setProperty(target, propertyName, orginValue);
 			}
 		}
+	}
+	
+	public static void main(String[] args) throws Exception {
+		// 2016年3月30日 17:54:50 看了PropertyUtils的实现，它支持NESTED的获取方式，支持数组获取方式[]，支持map的获取
+		InnerClass innerClass = new InnerClass();
+		innerClass.getInnerInnerClass().setName("nick");
+		innerClass.getInnerInnerClass().getScores().add(3);
+		innerClass.getInnerInnerClass().getMap().put("name", "karen");
+		System.out.println(PropertyUtils.getProperty(innerClass, "innerInnerClass.name"));
+		System.out.println(PropertyUtils.getProperty(innerClass, "innerInnerClass.scores[0]"));
+		System.out.println(PropertyUtils.getProperty(innerClass, "innerInnerClass.map.name"));
+		System.out.println(PropertyUtils.getProperty(innerClass, "innerInnerClass.map(name)")); // map写法也可以这样
+		/**
+		 * 看PropertyUtils的实现原理：
+		 * 它没有语法树，就是约定这些简单的层次.(NESTED)结构之后，一层一层的取。
+		 */
 	}
 
 }
