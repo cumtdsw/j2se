4f62494351453b785e5d2812b3325c3cd65bef6c
pugwoo <pugwoo@gmail.com>
2017-08-03 10:28:26 +0800

    clean

--- json/src/main/java/com/pugwoo/fastjson/User.java
+++ json/src/main/java/com/pugwoo/fastjson/User.java
@@ -2,12 +2,28 @@ package com.pugwoo.fastjson;
 
 import java.util.Date;
 
+import com.alibaba.fastjson.JSON;
+
 public class User {
 
 	private Integer id;
 	private String name;
 	private Date birth;
 	private transient int age;
+	
+	// XXX fastjson有问题：下划线开头的属性，序列化之后下划线不见了，得用@JSONField
+	private String _address;
+	
+	// 单元测试，
+	public static void main(String[] args) {
+		User user = new User();
+		user.setId(33);
+		user.setName("test");
+		user.setBirth(new Date());
+		user.set_address("test address");
+		
+		System.out.println(JSON.toJSONString(user, true));
+	}
 
 	public String toString() {
 		return "id:" + id + ",name:" + name + ",birth:" + birth + ",age:" + age;
@@ -43,6 +59,14 @@ public class User {
 
 	public void setAge(int age) {
 		this.age = age;
+	}
+
+	public String get_address() {
+		return _address;
+	}
+
+	public void set_address(String _address) {
+		this._address = _address;
 	}
 
 }
