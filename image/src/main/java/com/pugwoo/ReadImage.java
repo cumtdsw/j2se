100a6af08803c6e807cb7c4a581529be8c54c098
pugwoo <pugwoo@gmail.com>
2017-10-17 10:34:06 +0800

    ==

--- image/src/main/java/com/pugwoo/ReadImage.java
+++ image/src/main/java/com/pugwoo/ReadImage.java
@@ -10,7 +10,7 @@ public class ReadImage {
 	public static void main(String[] args) throws Exception {
 		String url = "https://www.baidu.com/img/bd_logo1.png";
 		BufferedImage image = ImageIO.read(new URL(url)); // 可以从网络上获取图片
-		// 可以从File  InputStream获取图片
+		// 可以从File InputStream获取图片
 		System.out.println("height:" + image.getHeight() + ",width:" + image.getWidth());
 	}
 	
