487a25e823ec73d185b5789d7105918517e45879
pugwoo <pugwoo@gmail.com>
2017-10-26 11:31:22 +0800

    ==

--- xml/src/com/pugwoo/JAXP/DomParse.java
+++ xml/src/com/pugwoo/JAXP/DomParse.java
@@ -1,7 +1,13 @@
 package com.pugwoo.JAXP;
 
+import java.io.File;
+
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
@@ -22,5 +28,11 @@ public class DomParse {
 	    for(int i = 0; i < list.getLength(); i++) {
 	    	System.out.println(list.item(i).getTextContent());
 	    }
+	    
+	    // 可以对document进行修改
+	    
+	    // document保存为xml文件
+	    Transformer transformer = TransformerFactory.newInstance().newTransformer();
+	    transformer.transform(new DOMSource(document), new StreamResult(new File("d:/gen.xml")));
 	}
 }
